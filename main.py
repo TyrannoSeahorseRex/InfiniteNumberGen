ad463b0c737ee7de129c9aac043ebaf1921f735a
Sea Rex <wheeler_harry@icloud.com>
2025-01-28 20:31:29 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+specialNumber = float(input("Please input your special number: "))
+xnum = 1
+znum = specialNumber * xnum
+
+while True:
+  print(f"{specialNumber} * {xnum} is {znum}")
+  xnum = xnum + 1
+  znum = specialNumber * xnum
